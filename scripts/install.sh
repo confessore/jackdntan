7192f5eeea2ec017c44222543aa9b8be57b01088
confessore <confessore@balasolu.com>
2022-03-04 10:32:09 -0500

    explicit move

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,8 @@
 
 sudo systemctl stop jackdntan.service
 sudo systemctl disable jackdntan.service
-cargo build --release --target-dir /var/jackdntan
+cargo build --release
+sudo mv ./target/release/jackdntan /var/jackdntan
 sudo cp ./services/jackdntan.service ./jackdntan.service.backup
 sudo mv ./services/jackdntan.service.backup /etc/systemd/system/jackdntan.service
 sudo systemctl enable jackdntan.service
