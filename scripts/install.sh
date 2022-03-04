46c4ec9b04cce5ed1bafe5af8781748671e4413f
confessore <confessore@balasolu.com>
2022-03-03 22:31:47 -0500

    initial commit

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo systemctl stop jackdntan.service
+sudo systemctl disable jackdntan.service
+cargo build --release --target-dir /var/jackdntan
+sudo cp ./services/jackdntan.service ./jackdntan.service.backup
+sudo mv ./services/jackdntan.service.backup /etc/systemd/system/jackdntan.service
+sudo systemctl enable jackdntan.service
+sudo systemctl start jackdntan.service
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let index = warp::path!().map(|| {
+        Command::new("cd").arg("~/balasolu").spawn().expect("could not change directories");
+        Command::new("git").arg("pull").spawn().expect("unable to git pull");
+        Command::new("docker-compose").arg("-f").arg("docker-compose-release.yml").arg("down").spawn().expect("unable to run docker command");
+        Command::new("docker-compose").arg("-f").arg("docker-compose-release.yml").arg("up").arg("-d").arg("--build").spawn().expect("unable to run docker command");
+        format!("{}", "")
+    });
+
+    warp::serve(index)
+        .run(([127, 0, 0, 1], 5321))
+        .await;
+}
