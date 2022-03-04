080a21e98b1033c72db2315a202fa9e14fb324e4
confessore <confessore@balasolu.com>
2022-03-04 11:05:38 -0500

    env dir

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let index = warp::path!().map(|| {
-        Command::new("cd").arg("~/balasolu").spawn().expect("could not change directories");
+        std::env::set_current_dir("~/balasolu").expect("could not change directories");
         Command::new("git").arg("pull").spawn().expect("unable to git pull");
         Command::new("sudo").arg("docker-compose").arg("-f").arg("docker-compose-release.yml").arg("down").spawn().expect("unable to run docker command");
         Command::new("sudo").arg("docker-compose").arg("-f").arg("docker-compose-release.yml").arg("up").arg("-d").arg("--build").spawn().expect("unable to run docker command");
