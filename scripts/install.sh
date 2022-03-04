6779408f918f0f68251fd39129ba902402a86913
confessore <confessore@balasolu.com>
2022-03-04 10:45:59 -0500

    service

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ sudo systemctl stop jackdntan.service
 sudo systemctl disable jackdntan.service
 cargo build --release
 sudo mv ./target/release/jackdntan /var/jackdntan
-sudo cp ./services/jackdntan.service ./jackdntan.service.backup
+sudo cp ./services/jackdntan.service ./services/jackdntan.service.backup
 sudo mv ./services/jackdntan.service.backup /etc/systemd/system/jackdntan.service
 sudo systemctl enable jackdntan.service
 sudo systemctl start jackdntan.service
