e17da020b28385d5f672fc9fdec1ed7687566280
confessore <confessore@balasolu.com>
2022-03-04 11:20:15 -0500

    implicit

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     let index = warp::path!().map(|| {
-        Command::new("git").arg("-C").arg("/home/$USER/balasolu").arg("pull").spawn().expect("unable to git pull");
-        Command::new("sudo").arg("docker-compose").arg("-f").arg("/home/$USER/balasolu/docker-compose-release.yml").arg("down").spawn().expect("unable to run docker command");
-        Command::new("sudo").arg("docker-compose").arg("-f").arg("/home/$USER/balasolu/docker-compose-release.yml").arg("up").arg("-d").arg("--build").spawn().expect("unable to run docker command");
+        Command::new("git").arg("-C").arg("~/balasolu").arg("pull").spawn().expect("unable to git pull");
+        Command::new("sudo").arg("docker-compose").arg("-f").arg("~/balasolu/docker-compose-release.yml").arg("down").spawn().expect("unable to run docker command");
+        Command::new("sudo").arg("docker-compose").arg("-f").arg("~/balasolu/docker-compose-release.yml").arg("up").arg("-d").arg("--build").spawn().expect("unable to run docker command");
         format!("{}", "")
     });
 
